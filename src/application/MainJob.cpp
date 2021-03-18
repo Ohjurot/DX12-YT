601e127051ee3925c5107ccf0c6a2bab83fc6000
Ludwig Füchsl <ludwig@fuechsl.org>
2021-03-18 18:36:04 +0100

    Revert resources...

--- src/application/MainJob.cpp
+++ src/application/MainJob.cpp
@@ -13,18 +13,6 @@
 #include <dx/cmds/CommandListManager.h>
 #include <dx/cmds/CommandListAccessObject.h>
 
-// DEBUG
-#include <engine/rxManager/RxAllocator.h>
-#include <engine/rxManager/Resource.h>
-#include <engine/rxManager/RxHash.h>
-#include <engine/rxManager/RxTools.h>
-// DEBUG
-
-bool resProc(_RX_DATA* ptrData, RX_PAGE_TYPE source, RX_PAGE_TYPE target, void* context) {
-
-	return true;
-}
-
 MAIN_JOB(ytDirectXMain) {
 	JOB_EXECUTE_FUNCTION(unsigned int index) {
 		// Enable and validate debug interface
@@ -43,9 +31,6 @@ MAIN_JOB(ytDirectXMain) {
 		DX::CommandQueueManager::getInstance().createInternalObjects(xDevice);
 		DX::CommandListManager::getInstance().createInternalObjects(xDevice);
 
-		// Init engine
-		engine::RxAllocator::init();
-
 		// Create window class
 		EasyHWND::WindowClass cls(L"MyWindowCls", CS_OWNDC, NULL, LoadCursor(NULL, IDC_ARROW), NULL, NULL, m_hInstance);
 		EXPP_ASSERT(cls, "Window class not valid");
@@ -57,30 +42,6 @@ MAIN_JOB(ytDirectXMain) {
 		// Create window
 		DX::GfxWindow window(cls, xDevice, factory2, L"DirectX 12", DX::GfxWindow_Stlye::BORDERLESS);
 		window.setWindowVisibility(true);
-
-		// === DEBUG
-
-		DX::XHeap heap(xDevice, MEM_MiB(1));
-
-		RESOURCE rx = engine::RxTools::createResource(L"Resource 1234", &resProc, &resProc, RX_PAGE_READ_ONLY, RX_PAGE_WRITE_ONLY, RX_PAGE_UNAVAILIBLE);
-		MessageBox(NULL, RESOURCE_NAME(rx), L"Name of Resource", MB_OK);
-
-		D3D12_RESOURCE_DESC buffer;
-		ZeroMemory(&buffer, sizeof(D3D12_RESOURCE_DESC));
-		buffer.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
-		buffer.Width = sizeof(unsigned char) * 256;
-		buffer.Height = 1;
-		buffer.DepthOrArraySize = 1;
-		buffer.MipLevels = 1;
-		buffer.SampleDesc.Count = 1;
-		buffer.SampleDesc.Quality = 0;
-		buffer.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
-
-		engine::RxTools::resourceAllocGpu(rx, xDevice, heap, 0, &buffer);
-
-		RX_UPDATE_RESULT res = engine::RxTools::resourcePageUpdate(rx, RX_PAGE_TYPE_DISK, RX_PAGE_TYPE_CPU);
-
-		// ===
 
 		// AO
 		DX::CommandListAccessObject lao(D3D12_COMMAND_LIST_TYPE_DIRECT);
@@ -95,17 +56,9 @@ MAIN_JOB(ytDirectXMain) {
 		}
 		lao.release();
 
-		// === 
-		engine::RxTools::resourceFreeGpu(rx);
-		heap.release();
-		// ===
-
 		// Destroy window
 		window.release();
 		factory2.release();
-
-		// Shutdown engine
-		engine::RxAllocator::shutdown();
 
 		// Release queues and device
 		DX::CommandListManager::getInstance().destroyInternalObjects();
